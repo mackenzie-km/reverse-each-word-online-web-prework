54817d2dd20fc600160faff16bb37c211d84c769
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-10 04:23:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,10 @@
+require "pry"
 def reverse_each_word(string)
   new_sentence = []
   sentence = string.split(" ")
   sentence.each do |word|
   new_sentence << word.reverse!
   new_sentence.join
+  binding.pry
   end
 end
