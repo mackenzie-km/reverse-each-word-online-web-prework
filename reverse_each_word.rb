0a73c5a0fafff35b735221a401c4f80f95d55efe
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-10 04:10:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(string)
   sentence = string.split("")
   sentence.each do |word|
-    reversed_sentence = sentence.reverse!
-    reversed_sentence
+  word.reverse!
   end
 end
