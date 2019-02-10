4db082276eb30e48e529e77da02caa8f4cb95cf8
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-10 04:27:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 require "pry"
 def reverse_each_word(string)
-  new_sentence = []
   sentence = string.split(" ")
   sentence.each do |word|
-  new_sentence << word.reverse!
+  word.reverse!
+  sentence.join
   binding.pry
   end
 end
